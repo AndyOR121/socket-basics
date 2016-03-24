4738b79b8090bf6a3998545094ce42824656df99
Andrew O'Riordan <andrew.oriordan.tqs@gmail.com>
2016-03-24 15:58:54 +0000

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room') || 'MainRoom';
 var socket = io();
 
 socket.on('connect', function() {
@@ -6,9 +8,11 @@ socket.on('connect', function() {
 
 socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>' + message.text + '</strong>  - ' + momentTimestamp.local().format('MMM Do YYYY, h:mm:ss a') + '</p>');
+	$message.append('<p><strong>' + message.name + '</strong>  - ' + momentTimestamp.local().format('MMM Do YYYY, h:mm:ss a') + '</p>');
+	$message.append('<p>' + message.text + '</p>')
 });
 
 var $form = jQuery('#message-form');
@@ -19,8 +23,22 @@ $form.on('submit', function(event) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
 	$message.val('');
 });
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    
+    return undefined;
+}

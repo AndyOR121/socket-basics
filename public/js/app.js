a7253e346fd32eb8999ff613a6bf8f7f00df8ee6
Andrew O'Riordan <andrew.oriordan.tqs@gmail.com>
2016-03-24 16:48:09 +0000

    Updated with rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,14 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room') || 'MainRoom';
 var socket = io();
 
+jQuery('.room-title').text(room);
+
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function(message) {
@@ -36,7 +42,7 @@ function getQueryVariable(variable) {
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split('=');
         if (decodeURIComponent(pair[0]) == variable) {
-            return decodeURIComponent(pair[1]);
+            return decodeURIComponent(pair[1].replace(/\+/g, ' '));
         }
     }
     
